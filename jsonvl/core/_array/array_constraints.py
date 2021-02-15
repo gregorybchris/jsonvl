718ca378abea5633483e6a059619d76df50c8175
gregorybchris <gregorybchris@gmail.com>
2021-02-15 04:31:10 -0800

    Add simple unique constraint to array types

--- jsonvl/core/_array/array_constraints.py
+++ jsonvl/core/_array/array_constraints.py
@@ -6,4 +6,4 @@ from jsonvl.utilities.venum import Venum
 class ArrayConstraints(Venum):
     """Constraints applied to array types."""
 
-    pass
+    UNIQUE = 'unique'
--- jsonvl/core/_array/array_validation.py
+++ jsonvl/core/_array/array_validation.py
@@ -1,6 +1,11 @@
 """Array validation."""
 from jsonvl.constants.reserved import Reserved
+from jsonvl.constants.builtins import Primitive, Collection
+from jsonvl.core._array.array_constraints import ArrayConstraints
 from jsonvl.exceptions.errors import ValidationError
+
+
+TYPE_NAME = Collection.ARRAY.value
 
 
 def validate_array(data, schema, validator, path):
@@ -20,3 +25,35 @@ def validate_array(data, schema, validator, path):
     for i, elem in enumerate(data):
         new_path = f'{path}[{i}]'
         validator._validate(elem, elem_schema, new_path)
+
+    if Reserved.CONSTRAINTS in schema:
+        type_constraints = schema[Reserved.CONSTRAINTS]
+        for cons_name, cons_value in type_constraints.items():
+            if not ArrayConstraints.has(cons_name):
+                raise ValidationError(f"The type {TYPE_NAME} has no constraint {cons_name}")
+
+            if cons_name == ArrayConstraints.UNIQUE.value:
+                _constrain_unique(cons_name, data, cons_value, path)
+            else:
+                raise ValidationError(f"The constraint {cons_name} is not implemented for type {TYPE_NAME}")
+
+
+def _constrain_unique(cons_name, data, value, path):
+    if isinstance(value, bool):
+        for x_i, x in enumerate(data):
+            for y_i, y in enumerate(data):
+                if x_i != y_i and x == y:
+                    raise ValidationError(f"Constraint \"{cons_name}\" was not met with "
+                                          f"{path}[{x_i}] == {path}[{y_i}]")
+    elif isinstance(value, str):
+        # TODO: Handle string which is one path that constraints one set of unique items
+        pass
+    elif isinstance(value, list):
+        # TODO: Handle list of paths, each path constraints its own set of unique items
+        pass
+    else:
+        raise ValidationError(f"The {cons_name} constraint value ({value}) "
+                              f"for the data {data} must be of "
+                              f"{Primitive.BOOLEAN.value}, "
+                              f"{Primitive.STRING.value}, or "
+                              f"{Collection.ARRAY.value} type")
