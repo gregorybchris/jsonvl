3edfe2ded03a9c20b21b66f56cb76ffc7e9e3206
gregorybchris <gregorybchris@gmail.com>
2021-02-15 01:29:33 -0800

    Add color and validation message to CLI

---
+++ jsonvl/cli/color.py
@@ -0,0 +1,24 @@
+"""Color printing."""
+
+
+class Color:
+    """Terminal colors."""
+
+    BLUE = '\x1b[34m'
+    CYAN = '\x1b[36m'
+    GREEN = '\x1b[32m'
+    ORANGE = '\x1b[33m'
+    RED = '\x1b[31m'
+    RESET = '\x1b[39m'
+
+
+def print_color(*args, color=None, **kwargs):
+    """
+    Print a message to the terminal in color.
+
+    :param color: The color to print.
+    """
+    if color is not None:
+        print(color, end="")
+    print(*args, **kwargs)
+    print(Color.RESET, end="")
--- jsonvl/cli/main.py
+++ jsonvl/cli/main.py
@@ -2,13 +2,19 @@
 import argparse
 
 from jsonvl import validate_file
+from jsonvl.cli.color import Color, print_color
 
 
 def run():
     """Run the jsonvl CLI."""
     args = _parse_args()
 
-    validate_file(args.data_filepath, args.schema_filepath)
+    try:
+        validate_file(args.data_filepath, args.schema_filepath)
+        print_color("Validation successful", color=Color.GREEN)
+    except Exception:
+        print_color("Validation failed", color=Color.RED)
+        raise
 
 
 def _parse_args():
