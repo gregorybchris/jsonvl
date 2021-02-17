fcd93682b2855a3e26a5e109d16cc3c1cb8d46a5
gregorybchris <gregorybchris@gmail.com>
2021-02-17 03:08:46 -0800

    Add autodoc for doc generation

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,3 @@
-from recommonmark.parser import CommonMarkParser
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -29,7 +27,7 @@ author = 'Chris Gregory'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon', 'recommonmark']
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. jsonvl documentation master file, created by
-   sphinx-quickstart on Wed Feb 17 01:56:18 2021.
+   sphinx-quickstart on Wed Feb 17 02:54:54 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
